7122f48dbbb188fa7b0e1689be11329661d274b3
Christian Iversen <ci@iversenit.dk>
2025-03-02 15:11:57 +0100

    + Make release.sh use new generate.py script

--- release.sh
+++ release.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+set -eu
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 <foo>"
+    exit 1
+fi
+
+TARGET_DIR="$1"
+FIRST_COMMIT="75eb23384b20acaca85e582c8ed4cfada4248616"
+
 set_version() {
     DIR=$1
     VER=$2
@@ -7,6 +17,12 @@ set_version() {
     sed -i -re "s|^(version):.+|\1: ${VER}|" ${DIR}/config.yaml
     sed -i -re "s|^  (.+): \"(ghcr.io/chrivers/bifrost):.+\"|  \1: \"\2:${VER}\"|" ${DIR}/build.yaml
 }
+
+./generate.py "$TARGET_DIR" templates/readme-stable.jinja "${FIRST_COMMIT}..master" > bifrost/README.md
+./generate.py "$TARGET_DIR" templates/readme-dev.jinja    "master..dev"             > bifrost-dev/README.md
+
+./generate.py "$TARGET_DIR" templates/changelog-stable.jinja "${FIRST_COMMIT}..master" > bifrost/CHANGELOG.md
+./generate.py "$TARGET_DIR" templates/changelog-dev.jinja    "dev..master"             > bifrost-dev/CHANGELOG.md
 
 set_version bifrost  master-2025-01-25
 set_version bifrost-dev dev-2025-01-30
